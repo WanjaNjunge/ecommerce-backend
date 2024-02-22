69b94536199b72eb4b3aff0688c16a0ea2321a21
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2024-02-22 12:13:05 +0300

    Add register user api with error handling and password hashing

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } =  require("mongoose");
 
 const dbConnect = () => {
     try {
-        const conn =  mongoose.connect("mongodb+srv://wnjunge19:gBmyopau0rd8LefZ@ecombe.ve0fh8a.mongodb.net/ecom-API?retryWrites=true&w=majority&appName=EcomBE");
+        const conn =  mongoose.connect(process.env.MONGODB_URL);
         console.log("Database connected successfully")
     } catch (error) {
         console.log("Database error")
