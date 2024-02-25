84181bb684b6c5504937a14a1620d3bd502cea48
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2024-02-25 14:58:40 +0300

    feat:add brand APIS

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const categoryRouter = require("./routes/categoryRoute");
+const brandRouter = require("./routes/brandRoute");
 const cookieParser  = require("cookie-parser");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/category", categoryRouter);
+app.use("/api/brand", brandRouter);
 
 
 app.use(notFound);
